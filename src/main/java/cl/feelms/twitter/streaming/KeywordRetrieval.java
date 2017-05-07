3919621c6bd14a08947e7c0ef57a4692c17958bf
ichigohollow <gonzalo.estay@usach.cl>
2017-05-07 04:16:25 -0300

    Se agrega la recolección de keywords desde la base de datos sql.

---
+++ src/main/java/cl/feelms/twitter/streaming/KeywordRetrieval.java
@@ -0,0 +1,48 @@
+package cl.feelms.twitter.streaming;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+public class KeywordRetrieval {
+
+public void conn()
+{
+    try {
+        //  Apertura de words.dat
+        String ruta = "./src/main/resources/words.dat";
+        BufferedWriter bw = new BufferedWriter(new FileWriter(ruta));
+        
+        //  Conexión a SQL
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        String urlConn = "jdbc:mysql://localhost:3306/feelms_sql?serverTimezone=UTC";
+        String usr = "root", pwd = "root";
+        String query = "SELECT term FROM key_terms";
+        Connection con = DriverManager.getConnection(urlConn, usr, pwd);
+        Statement stmt = con.createStatement();
+        ResultSet rset = stmt.executeQuery(query);
+        
+        //  Se itera sobre los resultados y se imprimen en words.dat
+        while(rset.next())
+        {
+            bw.write(rset.getString(1));
+            bw.newLine();
+        }
+        
+        //  Se cierra el archivo y la conexión a la bd.
+        bw.close();
+        con.close();
+    } catch (Exception e) {
+        System.out.println(e);
+    }
+    
+}    
+
+}
--- src/main/java/cl/feelms/twitter/streaming/TwitterStreaming.java
+++ src/main/java/cl/feelms/twitter/streaming/TwitterStreaming.java
@@ -24,6 +24,8 @@ import java.util.List;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.Date;
+import java.util.Calendar;
 
 import org.apache.commons.io.IOUtils;
 
@@ -36,109 +38,131 @@ import twitter4j.TwitterStream;
 import twitter4j.TwitterStreamFactory;
 
 import org.bson.Document;
+import twitter4j.TweetEntity;
 
 public class TwitterStreaming {
 
-	private final TwitterStream twitterStream;
-	private Set<String> keywords;
-	public static MongoConn mongoConn = new MongoConn();
+    private final TwitterStream twitterStream;
+    private Set<String> keywords;
+    public static MongoConn mongoConn = new MongoConn();
+    
 
-	private TwitterStreaming() {
-		this.twitterStream = new TwitterStreamFactory().getInstance();
-		this.keywords = new HashSet<>();
-		loadKeywords();
-	}
+    private TwitterStreaming() {
+        this.twitterStream = new TwitterStreamFactory().getInstance();
+        this.keywords = new HashSet<>();
+        loadKeywords();
+    }
 
-	private void loadKeywords() {
-		try {
-			ClassLoader classLoader = getClass().getClassLoader();
-			keywords.addAll(IOUtils.readLines(classLoader.getResourceAsStream("words.dat"), "UTF-8"));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    private void loadKeywords() {
+        try {
+            ClassLoader classLoader = getClass().getClassLoader();
+            keywords.addAll(IOUtils.readLines(classLoader.getResourceAsStream("words.dat"), "UTF-8"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	private void init() {
-		StatusListener listener = new StatusListener() {
+    private void init() {
+        StatusListener listener = new StatusListener() {
 
-			public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
-				System.out.println("Got a status deletion notice id:" + statusDeletionNotice.getStatusId());
-			}
+            public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
+                    System.out.println("Got a status deletion notice id:" + statusDeletionNotice.getStatusId());
+            }
 
-			public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
-				System.out.println("Got track limitation notice:" + numberOfLimitedStatuses);
-			}
+            public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
+                    System.out.println("Got track limitation notice:" + numberOfLimitedStatuses);
+            }
 
-			public void onScrubGeo(long userId, long upToStatusId) {
-				System.out.println("Got scrub_geo event userId:" + userId + " upToStatusId:" + upToStatusId);
-			}
+            public void onScrubGeo(long userId, long upToStatusId) {
+                    System.out.println("Got scrub_geo event userId:" + userId + " upToStatusId:" + upToStatusId);
+            }
 
-			public void onException(Exception ex) {
-				ex.printStackTrace();
-			}
+            public void onException(Exception ex) {
+                    ex.printStackTrace();
+            }
 
-			@Override
-			public void onStallWarning(StallWarning arg0) {
+            @Override
+            public void onStallWarning(StallWarning arg0) {
 
-			}
+            }
 
-			@Override
-			public void onStatus(Status status) {
-				/*
-				System.out.println(status.getId());
-				System.out.println(status.getText());
-				*/
+            @Override
+            public void onStatus(Status status) {
+                /* 	Usar esto para filtrar países, por ahora solo se obtienen tweets en
+                        español, independiente de la ubicación.
+                if(status.getPlace() != null)
+                {
+                        System.out.println(status.getPlace().getCountryCode());
+                }
+                */
+                
+                
+                
+                if(status.getLang().equals("es"))
+                {
+                    Date fecha = status.getCreatedAt();
+                    Calendar cal = Calendar.getInstance();
+                    //cal.setTime(fecha);
 
-				/* 	Usar esto para filtrar países, por ahora solo se obtienen tweets en
-					español, independiente de la ubicación.
-				if(status.getPlace() != null)
-				{
-					System.out.println(status.getPlace().getCountryCode());
-				}
-				*/
-				if(status.getLang().equals("es"))
-				{
+                    int yearT = cal.get(Calendar.YEAR);
+                    int monthT = cal.get(Calendar.MONTH) + 1;
+                    int dayT = cal.get(Calendar.DAY_OF_MONTH);
 
-					Document tweet = new Document("id", status.getId())
-										.append("user", status.getUser().getScreenName())
-										.append("name", status.getUser().getName())
-	               						.append("text", status.getText())
-										.append("rt_count", status.getRetweetCount())
-                                                                .append("fecha", status.getCreatedAt().toString());
+                    ArrayList<String> ht = new ArrayList<String>();
+                
+                    for (TweetEntity e : status.getHashtagEntities()) {
+                        ht.add(e.getText());
+                    }
+                    
+                    Document tweet = new Document("id", status.getId())
+                                        .append("user", status.getUser().getScreenName())
+                                        .append("name", status.getUser().getName())
+                                        .append("text", status.getText())
+                                        .append("rt_count", status.getRetweetCount())
+                                        .append("fecha", yearT+"-"+monthT+"-"+dayT)
+                                        .append("hashtag", ht);
 
-					mongoConn.getMColl().insertOne(tweet);
-				}
-			}
-		};
+                    mongoConn.getMColl().insertOne(tweet);
+                }
+            }
+        };
 
-		FilterQuery fq = new FilterQuery();
+        FilterQuery fq = new FilterQuery();
 
-		fq.track(keywords.toArray(new String[0]));
+        fq.track(keywords.toArray(new String[0]));
 
-		this.twitterStream.addListener(listener);
-		this.twitterStream.filter(fq);
-	}
+        this.twitterStream.addListener(listener);
+        this.twitterStream.filter(fq);
+    }
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        /*
+            Obtener keywords desde BD
+            Guardar en words.dat?
+        */
+        int i = 0;
+        
+        KeywordRetrieval kr = new KeywordRetrieval();
+        kr.conn();
 
-                //  Usuario
-		String user = "";
-                //  BD donde está el usuario
- 		String database = "";
-                //  Pass como arreglo de caracteres, ej: 
-                //  si la contraseña es pass, se escribe como {'p', 'a', 's', 's'}
- 		char[] password = {};
+        //  Usuario
+        String user = "admin";
+        //  BD donde está el usuario
+        String database = "admin";   
+        //  Pass como arreglo de caracteres, ej: 
+        //  si la contraseña es pass, se escribe como {'p', 'a', 's', 's'}
+        char[] password = {'a', 'd', 'm', 'i', 'n', '1', '2', '3'};
 
- 		mongoConn.setMC(MongoCredential.createCredential(user, database, password));
+        mongoConn.setMC(MongoCredential.createCredential(user, database, password));
 
- 		mongoConn.setMCl(new MongoClient(new ServerAddress("localhost", 27017),
-								Arrays.asList(mongoConn.getMC())));
+        mongoConn.setMCl(new MongoClient(new ServerAddress("localhost", 27017),
+                                                        Arrays.asList(mongoConn.getMC())));
 
-		mongoConn.setMDB(mongoConn.getMCl().getDatabase("feelms"));
+        mongoConn.setMDB(mongoConn.getMCl().getDatabase("feelms"));
 
-		mongoConn.setMColl(mongoConn.getMDB().getCollection("tweets"));
+        mongoConn.setMColl(mongoConn.getMDB().getCollection("tweets"));
 
-		new TwitterStreaming().init();
-	}
+        new TwitterStreaming().init();
+    }
 
 }
